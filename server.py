a0f18ffd27e644190bc4017b5d4e29a9e1c553c8
Edi Wu <eddie.wu.info@gmail.com>
2024-02-27 17:03:25 -0800

    Implement reading and saving image

--- server.py
+++ server.py
@@ -1,31 +1,47 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# Specify port for HTTP server
 PORT = 8000
 
 
+# Subclass the base handler to implement custom GET and POST handlers
 class HTTPRequestHandler(BaseHTTPRequestHandler):
 
+    # Basic GET handler that acknowledges request
     def do_GET(self):
         print("Incoming GET request.")
         for header, value in self.headers.items():
             print(f"{header}: {value}")
-        self.send_response(200)
-        self.end_headers()
+        self.send_response(200) # Send 200 OK code
+        self.end_headers() # Signal end of response headers
+        # Use 'wfile' stream to return response message with default encoding
         self.wfile.write("GET Request received".encode())
 
+    # Handles POST requests that send an image via 'curl'
+    # 'curl' command: curl -X POST --data-binary "@/image_path" http://localhost:PORT
     def do_POST(self):
         print("Incoming POST request.")
+        # Get content type of incoming request
         content_type = self.headers.get("Content-Type")
-        if content_type.startswith('multipart/form-data'):
-            print("Content-Type: multipart/form-data")
+        # Get length of data content
+        content_length = int(self.headers.get("Content-Length"))
+        # Read entire binary data
+        data = self.rfile.read(content_length)
+        # Write data into a jpeg file
+        with open("img.jpeg", "wb") as img:
+            img.write(data)
+        # Send response
         self.send_response(200)
         self.end_headers()
         self.wfile.write("POST Request received".encode())
 
 
+# Runs http server on specified port
 def run(server_class=HTTPServer, handler_class=HTTPRequestHandler):
     server_address = ('', PORT)
+    # Constructs server, specifying address and handler
     httpd = server_class(server_address, handler_class)
+    # Start the server (polls for requests at regular intervals)
     httpd.serve_forever()
 
 
